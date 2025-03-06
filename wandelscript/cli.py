a988362ba86f5ef88f10f04c0aecdb5d05aed57b
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-03-06 16:03:37 +0100

    feat(RPS-1286): Add Wandelscript CLI executable
    Just a small one.
    
    At the moment, only accesses a cell, everything else has to be defined
    inside the wandelscript.
    E.g., code like the following is supposed to work, assuming the cell and
    TCPs are set up prior:
    
        tcp("flange")
        robot = get_controller("controller")[0]
        home = read(robot, "pose")
        sync
    
        # Set the velocity of the robot to 200 mm/s
        velocity(200)
    
        do with robot:
            for i = 0..3:
                move via ptp() to home
                move via line() to (0, 0, 0, 0, 0, 0) :: home
                move via line() to (200, 0, 0, 0, 0, 0) :: home
                move via ptp() to home
    
    I see that in future we can specify cell, robot, TCPs and more via
    command line. However, baby steps. Thus adding this now hereby.
    I believe we can grow this over time and according to our usage
    patterns.
    
    Install the CLI tool by calling:
    
        poetry install
    
    This installs the executable into your virtual environment.
    
    You can then use the CLI via:
    
        poetry run wandelscript --help
        poetry run wandelscript examples/my_script.cli
        poetry run ws examples/my_script.cli  # shortcut alternative
    
    I used `typer` for its apparent popularity within the team.
    
    Note that importing wandelscript takes a horrendous amount of time,
    thus the CLI loads precariously slow.

---
+++ wandelscript/cli.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""CLI tool to work with Wandelscript."""
+
+import asyncio
+from pathlib import Path
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+from nova import Nova
+from typer import Exit, FileText, Option, Typer, echo
+
+import wandelscript
+
+load_dotenv()
+
+app = Typer()
+
+
+def _validate_url(url: str) -> bool:
+    """Validate the provided URL. Return True if valid, otherwise return False."""
+    parsed = urlparse(url)
+    if parsed.scheme and parsed.netloc:
+        return True
+    return False
+
+
+async def main(code: str):
+    """Main program logic"""
+    async with Nova() as nova:
+        cell = nova.cell()
+        robot_cell = await cell.get_robot_cell()
+        runner = wandelscript.run(code, robot_cell=robot_cell, default_tcp=None, default_robot=None)
+
+    echo(runner.program_run.execution_results)
+
+
+@app.command()
+def run(script: FileText, nova_api: str = Option(None, "--nova-api", "-n", envvar="NOVA_API", help="URL to NOVA API")):
+    """Run Wandelscript programs."""
+
+    if not nova_api:
+        echo("Error: NOVA_API must be set via '--nova-api' or as an environment variable", err=True)
+        raise Exit(1)
+    if not _validate_url(nova_api):
+        echo(f"Error: NOVA_API value {nova_api} is not a valid URL", err=True)
+        raise Exit(1)
+
+    echo(f"NOVA_API: {nova_api}")
+
+    code = script.read()
+    script.close()
+
+    asyncio.run(main(code))
+
+
+if __name__ == "__main__":
+    app()
